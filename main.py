2ebe884b5709c27af92016fb3906a3820a3da2c7
Eric <Eric>
2023-05-05 16:07:09 -0300

    Initial commit.

---
+++ main.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.pack()
+#   timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
+
+
+
+window.mainloop()
+
+
+
