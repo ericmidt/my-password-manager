66a599a8ee7ca52ea888c67e656319e16728bb7e
Eric <Eric>
2023-05-09 20:59:54 -0300

    Add search feature.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -31,21 +32,57 @@ def generate_password():
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
 
-    if website.get() == "" or username.get() == "" or password.get() == "":
+    url = website.get().capitalize()
+    user = username.get()
+    pwd = password.get()
+
+    new_data = {
+        url: {
+            "email": user,
+            "password": pwd
+        }
+    }
+
+    if url == "" or user == "" or pwd == "":
         messagebox.showinfo(title="Error",  message="Please don't leave any fields empty.")
         return
 
     is_ok = messagebox.askokcancel(title="Confirmation", message=f"Would you like to save the following password?\n\n"
                                                                  f"Website: {website.get()}\nEmail: {username.get()}"
                                                                  f"\nPassword: {password.get()}")
-
     if is_ok:
-        pyperclip.copy(password.get())
-        with open("saved_passwords.txt", "a") as f:
-            f.write(f"{website.get()} | {username.get()} | {password.get()}\n")
+
+        try:
+            with open("saved_passwords.json", "r") as f:
+                data = json.load(f)
+                data.update(new_data)
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            with open("saved_passwords.json", "w") as f:
+                json.dump(new_data, f, indent=4)
+        else:
+            with open("saved_passwords.json", "w") as f:
+                json.dump(data, f, indent=4)
+        finally:
+            pyperclip.copy(pwd)
             website.delete(0, END)
             password.delete(0, END)
             website.focus()
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    url = website.get().capitalize()
+    try:
+        with open("saved_passwords.json", "r") as f:
+            data = json.load(f)
+            if url in data:
+                messagebox.showinfo(title="Password information\n",
+                                    message=f"Website: {url}\nEmail: {data[url]['email']}\nPassword: {data[url]['password']}")
+                pyperclip.copy(data[url]['password'])
+            else:
+                messagebox.showinfo(title="Error\n", message=f"There are no passwords saved for this website.")
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        messagebox.showinfo(title="Error\n", message=f"No data found.")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -61,8 +98,11 @@ text_1 = Label(text="Website:")
 text_1.grid(row=1, column=0)
 
 website = Entry(width=10)
-website.grid(row=1, column=1, columnspan=2, pady=1, sticky="ew")
+website.grid(row=1, column=1, pady=1, sticky="ew")
 website.focus()
+
+button0 = Button(text="Search", command=find_password)
+button0.grid(row=1, column=2, sticky="ew")
 
 text_2 = Label(text="Email/Username:")
 text_2.grid(row=2, column=0)
@@ -84,6 +124,3 @@ button = Button(text="Add", command=save_password)
 button.grid(row=4, column=1, columnspan=2, sticky="ew")
 
 window.mainloop()
-
-
-
